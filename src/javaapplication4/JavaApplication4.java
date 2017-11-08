dd4462d72d6402db670d197e9dfcce6207723887
B1 <B1@B148>
2017-11-08 13:17:38 +0800

    [Delivers#152266834]

--- src/javaapplication4/JavaApplication4.java
+++ src/javaapplication4/JavaApplication4.java
@@ -15,7 +15,7 @@ public class JavaApplication4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("asdasdadasdaasdadsdadasd");
+        System.out.println("asdasdadasdaasdadadasd");
     }
     
 }
